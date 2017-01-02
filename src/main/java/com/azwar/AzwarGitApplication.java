33edad5acedc22b0ce09c474a7756f5d59e8e4df
AzwarFX <data.azwarazuhri@gmail.com>
2017-01-02 11:07:06 +0700

    first commit

---
+++ src/main/java/com/azwar/AzwarGitApplication.java
@@ -0,0 +1,12 @@
+package com.azwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzwarGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzwarGitApplication.class, args);
+	}
+}
